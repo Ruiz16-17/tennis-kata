b7c8018f4ff00889392c60e2fcd61b50eb4739d8
Sebastian Ruiz Gallego <sebasruigalle62@gmail.com>
2021-09-23 23:51:08 -0500

    Refactor in Tennisgame1, type winner, winner

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,8 +1,8 @@
 
 public class TennisGame1 implements TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+
+    private int player1Score = 0;
+    private int player2Score = 0;
     private String player1Name;
     private String player2Name;
 
@@ -12,65 +12,93 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName == this.player1Name) {
+            player1Score += 1;
+        }
+
+            player2Score += 1;
+
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
+        int tempScore = 0;
+        if (player1Score == player2Score) {
+            score = getEqualsLessThreeScoreString(player1Score);
+        } else if (player1Score >= 4 || player2Score >= 4) {
+
+            score = getScoreGreaterThreeString(player1Score,player2Score);
+
+        } else {
+            for (int i = 1; i < 3; i++) {
+                if (i == 1) tempScore = player1Score;
+                else {
+                    score += "-";
+                    tempScore = player2Score;
                 }
+                score += getScoreLessFourString(tempScore);
             }
         }
         return score;
     }
+
+    private String getEqualsLessThreeScoreString(int tempScore) {
+
+        String scoreString = "Deuce";
+
+        if (tempScore < 3) {
+
+            String[] scoreStringArray = new String[]{"Love-All", "Fifteen-All", "Thirty-All"};
+            scoreString = scoreStringArray[tempScore];
+        }
+
+        return scoreString;
+    }
+
+    private String getScoreLessFourString(int tempScore) {
+
+        String[] scoreString = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+
+        return scoreString[tempScore];
+    }
+
+    private String getScoreGreaterThreeString(int playerScore1, int playerScore2) {
+
+
+        return getTypeWinner(getDifferenceScore(playerScore1,playerScore2))
+                + " " +
+                getNameWinner(playerScore1,playerScore2);
+    }
+
+    private int getDifferenceScore(int playerScore1, int playerScore2){
+
+        return Math.abs(playerScore1 - playerScore2);
+
+    }
+
+    private String getTypeWinner(int differenceScore) {
+
+        String typeWinner = "Win for";
+
+        if(differenceScore == 1){
+
+            typeWinner = "Advantage";
+        }
+
+        return typeWinner;
+    }
+
+    private String getNameWinner(int playerScore1, int playerScore2) {
+
+        String nameWinner = this.player1Name;
+
+        if(playerScore1 < playerScore2){
+
+            nameWinner = this.player2Name;
+        }
+
+        return nameWinner;
+    }
+
+
 }
