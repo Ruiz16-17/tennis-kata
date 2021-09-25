2bf802485145c828cf7caef91da55783902e0eff
Sebastian Ruiz Gallego <sebasruigalle62@gmail.com>
2021-09-24 23:34:56 -0500

    Extract methods and created cases

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -1,133 +1,87 @@
+import java.util.Objects;
 
-public class TennisGame2 implements TennisGame
-{
-    public int P1point = 0;
-    public int P2point = 0;
-    
-    public String P1res = "";
-    public String P2res = "";
-    private String player1Name;
-    private String player2Name;
+public class TennisGame2 implements TennisGame {
+
+    public int player1Points = 0;
+    public int player2Points = 0;
+
+    private final String[] scoreStringArrayEquals = new String[]{"Love-All", "Fifteen-All", "Thirty-All"};
+    private final String[] scoreStringArray = new String[]{"Love", "Fifteen", "Thirty","Forty"};
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
     }
 
-    public String getScore(){
+    public String getScore() {
         String score = "";
-        if (P1point == P2point && P1point < 4)
-        {
-            if (P1point==0)
-                score = "Love";
-            if (P1point==1)
-                score = "Fifteen";
-            if (P1point==2)
-                score = "Thirty";
-            score += "-All";
+
+        if (isEqualsScores()) {
+            score = getStringScoreEqualsAndLessFour();
         }
-        if (P1point==P2point && P1point>=3)
-            score = "Deuce";
-        
-        if (P1point > 0 && P2point==0)
-        {
-            if (P1point==1)
-                P1res = "Fifteen";
-            if (P1point==2)
-                P1res = "Thirty";
-            if (P1point==3)
-                P1res = "Forty";
-            
-            P2res = "Love";
-            score = P1res + "-" + P2res;
+
+        if(isLessFourScores(4) && !isEqualsScores()) {
+
+            score = scoreStringArray[player1Points] + "-" + scoreStringArray[player2Points];
+
         }
-        if (P2point > 0 && P1point==0)
-        {
-            if (P2point==1)
-                P2res = "Fifteen";
-            if (P2point==2)
-                P2res = "Thirty";
-            if (P2point==3)
-                P2res = "Forty";
-            
-            P1res = "Love";
-            score = P1res + "-" + P2res;
+
+        if (player1Points > player2Points && player2Points >= 3) {
+            score = "Advantage " + this.player1Name;
         }
-        
-        if (P1point>P2point && P1point < 4)
-        {
-            if (P1point==2)
-                P1res="Thirty";
-            if (P1point==3)
-                P1res="Forty";
-            if (P2point==1)
-                P2res="Fifteen";
-            if (P2point==2)
-                P2res="Thirty";
-            score = P1res + "-" + P2res;
+
+        if (player2Points > player1Points && player1Points >= 3) {
+            score = "Advantage " + this.player2Name;
         }
-        if (P2point>P1point && P2point < 4)
-        {
-            if (P2point==2)
-                P2res="Thirty";
-            if (P2point==3)
-                P2res="Forty";
-            if (P1point==1)
-                P1res="Fifteen";
-            if (P1point==2)
-                P1res="Thirty";
-            score = P1res + "-" + P2res;
+
+        if (player1Points >= 4 && player2Points >= 0 && (player1Points - player2Points) >= 2) {
+            score = "Win for " + this.player1Name;
         }
-        
-        if (P1point > P2point && P2point >= 3)
-        {
-            score = "Advantage player1";
-        }
-        
-        if (P2point > P1point && P1point >= 3)
-        {
-            score = "Advantage player2";
-        }
-        
-        if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
-        {
-            score = "Win for player1";
-        }
-        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
-        {
-            score = "Win for player2";
+        if (player2Points >= 4 && player1Points >= 0 && (player2Points - player1Points) >= 2) {
+            score = "Win for " + this.player2Name;
         }
         return score;
     }
-    
-    public void SetP1Score(int number){
-        
-        for (int i = 0; i < number; i++)
-        {
-            P1Score();
+
+    //region General Comparisons
+
+    private boolean isEqualsScores() {
+
+        return this.player1Points == this.player2Points;
+    }
+
+    private boolean isLessFourScores(int limit) {
+
+        return this.player1Points < limit && this.player2Points < limit;
+    }
+
+    //endregion
+
+    private String getStringScoreEqualsAndLessFour() {
+
+        String score = "Deuce";
+
+        if (player1Points < 3) {
+
+            score = scoreStringArrayEquals[this.player1Points];
+
         }
-            
+
+        return score;
     }
-    
-    public void SetP2Score(int number){
-        
-        for (int i = 0; i < number; i++)
-        {
-            P2Score();
-        }
-            
+
+    private void P1Score() {
+        player1Points++;
     }
-    
-    public void P1Score(){
-        P1point++;
-    }
-    
-    public void P2Score(){
-        P2point++;
+
+    private void P2Score() {
+        player2Points++;
     }
 
     public void wonPoint(String player) {
-        if (player == "player1")
+        if (Objects.equals(player, this.player1Name))
             P1Score();
         else
             P2Score();
